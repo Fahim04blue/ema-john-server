6f2a99238d3c1de207d770a22b173a7addc3184a
Fahim <Shahriarfahim19@gmail.com>
2021-03-28 23:48:25 +0600

    aaa

--- server.js
+++ server.js
@@ -62,10 +62,9 @@ client.connect((err) => {
       res.send(result.insertedCount > 0);
     });
   });
-});
-
-app.get("/", function (req, res) {
-  res.send("hello world");
+  app.get("/", function (req, res) {
+    res.send("hello world");
+  });
 });
 
 app.listen(process.env.PORT || 5000, () => console.log("Listening to 5000"));
